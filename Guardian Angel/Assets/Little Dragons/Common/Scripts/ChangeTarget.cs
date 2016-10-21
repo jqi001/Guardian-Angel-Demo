bc505759eca10744fc4489829645331d017b333b
Marco Knietzsch <marco.knietzsch@rezahas.com>
2016-10-21 11:40:15 +0200

    added demo stuff

---
+++ Guardian Angel/Assets/Little Dragons/Common/Scripts/ChangeTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTarget : MonoBehaviour {
+
+    public Transform[] targets;
+    public KeyCode key = KeyCode.T;
+    int current;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            current++;
+            GetComponent<UnityStandardAssets.Cameras.FreeLookCam>().SetTarget(targets[(current% targets.Length)]);
+        }
+	}
+}
